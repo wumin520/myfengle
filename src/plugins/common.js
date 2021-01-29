0bab32d9236bfb7eeb0b2bab6a16890ae665694a
王志鹏 <wangzhipeng@ssish.com>
2021-01-29 13:42:16 +0800

    海报分享功能

--- src/plugins/common.js
+++ src/plugins/common.js
@@ -3,8 +3,9 @@ MyPlugin.install = function (wepy, options) {
   // 1. 添加全局方法或属性
   wepy.myGlobalMethod = function () {
     // 逻辑...
+
   }
-  
+
   let systemInfo = wx.getSystemInfoSync()
   let modelmes = systemInfo.model;
   let isIphoneX = false;
@@ -16,35 +17,38 @@ MyPlugin.install = function (wepy, options) {
   wepy.mixin({
     created: function () {
       // 逻辑...
+      wx.showShareMenu({
+        menus: ['shareAppMessage', 'shareTimeline']
+      });
     },
     data: {
-        isIphoneX,
-        statusBarHeight
+      isIphoneX,
+      statusBarHeight
     },
     methods: {
-        startTrack (sensors) {
-          sensors = sensors || getApp().sensors;
-          const openId = wx.getStorageSync('openId');
-          if (openId) {
-            sensors.setOpenid(openId);
-            sensors.init(); // 神策初始化
-          }
-        },
-        trackEvent (params) {
-            const [section_name, module_name, module_element_name] = params;
-            getApp().sensors.track('newbees_applets_click',{
-                section_name,
-                module_name,
-                module_element_name,
-            });
-        },
-        toast(title) {
-            wx.showToast({
-                title, //提示的内容,
-                icon: 'none', //图标
-                duration: 3000
-            })
-        },
+      startTrack(sensors) {
+        sensors = sensors || getApp().sensors;
+        const openId = wx.getStorageSync('openId');
+        if (openId) {
+          sensors.setOpenid(openId);
+          sensors.init(); // 神策初始化
+        }
+      },
+      trackEvent(params) {
+        const [section_name, module_name, module_element_name] = params;
+        getApp().sensors.track('newbees_applets_click', {
+          section_name,
+          module_name,
+          module_element_name,
+        });
+      },
+      toast(title) {
+        wx.showToast({
+          title, //提示的内容,
+          icon: 'none', //图标
+          duration: 3000
+        })
+      },
     }
   })
 }
